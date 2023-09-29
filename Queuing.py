435d0db9931e4e792f32bd08a6a7bbf3f9d7d3af
Abhigyan Sen <62685639+AbhigyanSen@users.noreply.github.com>
2023-09-29 15:06:47 +0530

    Corrupt Frame Problem Solved
    Error: Received Corrupt Frames for over 30secs SOLVED
    To Look Upon: Retry Times, MaxRetryTime

--- Queuing.py
+++ Queuing.py
@@ -4,68 +4,69 @@
 import cv2
 import time
 import logging
+import numpy as np
 import os
 import queue 
 import threading
 
+def FrameCorrupt(Frame):
+    AveragePixelValue = np.mean(Frame)
+    CorruptThreshold = 10
+    return AveragePixelValue < CorruptThreshold
+
 # Establish Connection
-def Connection(RtspURL, MaxRetries=3, RetryInterval=10, MaxCorruptFrameDuration=30):
-    Retries = 0
-    CorruptFrameStartTime = None
-
-    # Create a queue to hold frames
-    FrameQueue = queue.Queue()
-
-    def SaveFrames():
-        # Create the "data" folder if it doesn't exist
-        if not os.path.exists("data"):
-            os.makedirs("data")
-
-        FrameCount = 0
-        while True:
-            try:
-                frame = FrameQueue.get()
-                if frame is not None:
-                    FrameCount += 1
-                    # Save the frame as an image in the "data" folder
-                    filename = os.path.join("data", f"frame_{FrameCount}.jpg")
-                    cv2.imwrite(filename, frame)
-            except Exception as e:
-                logging.exception(e)
-
-    # Start the frame saving thread
-    SaveThread = threading.Thread(target=SaveFrames)
-    SaveThread.daemon = True
-    SaveThread.start()
-
-    while Retries < MaxRetries:
+def Connection(RtspURL, MaxRetries=3, RetryInterval=10, MaxCorruptFrameDuration=30):  
+    for Retry in range(MaxRetries):
         try:
             # Connect to the RTSP stream
-            Video = cv2.VideoCapture(RtspURL)
-
-            if not Video.isOpened():
-                print("Failed to Open RTSP Stream. Retrying in {} seconds.".format(RetryInterval))
+            cap = cv2.VideoCapture(RtspURL)
+            
+            if not cap.isOpened():
+                print("Failed to Open RTSP Stream. Retrying in {} seconds (Retry {}/{})".format(RetryInterval, Retry + 1, MaxRetries))
                 time.sleep(RetryInterval)
                 continue
 
-            while True:
-                _, Frame = Video.read()
+            CorruptFrameStartTime = None
+            
+            FrameQueue = queue.Queue()
 
+            def SaveFrames():
+                if not os.path.exists("test"):
+                    os.makedirs("test")
+
+                FrameCount = 0
+                while True:
+                    try:
+                        frame = FrameQueue.get()
+                        if frame is not None:
+                            FrameCount += 1
+                            # Saving the Frames
+                            FileName = os.path.join("test", f"frame_{FrameCount}.jpg")
+                            cv2.imwrite(FileName, frame)
+                    except Exception as e:
+                        logging.exception(e)
+
+            # Start the Frame Saving thread
+            SaveThread = threading.Thread(target=SaveFrames)
+            SaveThread.daemon = True
+            SaveThread.start()
+            
+            while True:
+                ret, Frame = cap.read()
+                
                 # Handling Camera Shutdown
-                if _ is False:
-                    print("No video feed available. Retrying in {} seconds.".format(RetryInterval))
-                    time.sleep(RetryInterval)
-                    continue
+                if not ret:
+                    print("No video feed available. Retrying in {} seconds (Retry {}/{})".format(RetryInterval, Retry + 1, MaxRetries))
+                    break
                 
                 # Handling Corrupt Frames
-                if CorruptFrameStartTime is None:
-                    CorruptFrameStartTime = time.time()
-                else:
-                    if (time.time() - CorruptFrameStartTime) >= MaxCorruptFrameDuration:
-                        print("Corrupt frames received for over {} seconds.".format(MaxCorruptFrameDuration))
+                if FrameCorrupt(Frame):
+                    if CorruptFrameStartTime is None:
+                        CorruptFrameStartTime = time.time()
+                    elif (time.time() - CorruptFrameStartTime) >= MaxCorruptFrameDuration:
+                        print("Corrupt frames received for over {} seconds. Retry (Retry {}/{})".format(MaxCorruptFrameDuration, Retry + 1, MaxRetries))
                         break
-
-                # Enqueue the frame
+                        
                 FrameQueue.put(Frame)
 
                 cv2.imshow("Video", Frame)
@@ -73,22 +74,24 @@ def Connection(RtspURL, MaxRetries=3, RetryInterval=10, MaxCorruptFrameDuration=
                 if k == ord('q'):
                     break
 
-            Video.release()
+            cap.release()
             cv2.destroyAllWindows()
-            break  # Connection successful, exit the retry loop
+            if ret:
+                print("Connection established successfully.")
+                break  # Connection successful, exit the Retry loop
+            else:
+                Retry = 0
+                time.sleep(RetryInterval)
 
         except Exception as e:
             logging.exception(e)
-            Retries += 1
+            Retry += 1
             time.sleep(RetryInterval)
 
-    if Retries == MaxRetries:
+    if Retry == MaxRetries:
         print("Failed to establish connection after {} Retries.".format(MaxRetries))
-    else:
-        print("Connection established successfully.")
 
-# Define RTSP URL and call the function
+# Example Case
 RtspURL = "rtsp://admin:Sheraton123@10.111.111.106:554/Streaming/Channels/501/"
 
-# Call the function with the RTSP URL
 Connection(RtspURL)
